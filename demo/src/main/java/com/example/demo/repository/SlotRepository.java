fc1996c2bac03121052f01ec9f1715e3bfafd68d
lakshmidhar2006 <santimallalakshmidhar@gmail.com>
2025-09-24 14:39:46 +0530

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/SlotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Slot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface SlotRepository extends JpaRepository<Slot, Long> {
-    List<Slot> findByParkingFloorId(Long floorId);
-}
